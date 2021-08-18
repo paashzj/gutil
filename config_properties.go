17a81567dca120e5aa04e7832b9f7c4e9d84b5f1
shoothzj <shoothzj@gmail.com>
2021-08-18 16:02:31 +0800

    Allow set int/int64 properties, read prop from file

--- config_properties.go
+++ config_properties.go
@@ -2,12 +2,41 @@ package gutil
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type ConfigProperties struct {
 	properties map[string]string
+}
+
+func ConfigPropFromFile(file string) (*ConfigProperties, error) {
+	configProp := ConfigProperties{}
+	configProp.Init()
+	fileBytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	split := strings.Split(string(fileBytes), "\n")
+	for _, line := range split {
+		trimLine := strings.TrimSpace(line)
+		if len(trimLine) == 0 {
+			continue
+		}
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+		array := strings.Split(line, "=")
+		if len(array) != 2 {
+			return nil, errors.New(fmt.Sprintf("line %s has multi = ", line))
+		}
+		configProp.Set(array[0], array[1])
+	}
+	return &configProp, nil
 }
 
 func (c *ConfigProperties) Init() {
@@ -16,6 +45,14 @@ func (c *ConfigProperties) Init() {
 
 func (c *ConfigProperties) Set(key, val string) {
 	c.properties[key] = val
+}
+
+func (c *ConfigProperties) SetInt(key string, val int) {
+	c.properties[key] = strconv.Itoa(val)
+}
+
+func (c *ConfigProperties) SetInt64(key string, val int64) {
+	c.properties[key] = strconv.FormatInt(val, 10)
 }
 
 func (c *ConfigProperties) Write(dstFile string) error {
